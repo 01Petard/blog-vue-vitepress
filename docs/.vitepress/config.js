de98861dacfab77ef571a883ca23baf861f4d203
Snxy <892573319@qq.com>
2024-06-17 02:07:31 +0800

    chore: 修改base路径

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,11 +1,14 @@
 // .vitepress/config.js
+
+const base = "/blog-demo/";
 export default {
+  base,
   // 站点级选项
   title: "是柠新呀的博客",
   description: "是柠新呀用来写博客的地方",
   head: [
     // 配置网站的图标（显示在浏览器的 tab 上）
-    ["link", { rel: "icon", href: `/favicon.ico` }],
+    ["link", { rel: "icon", href: `${base}favicon.ico` }],
   ],
   themeConfig: {
     docFooter: {
